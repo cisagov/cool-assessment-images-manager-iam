a07a56803e6d26c2efa5404ff1672ca48cccc983
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-04-03 15:30:51 -0400

    Remove all remaining production and staging specific references
    Since we no longer have to specify which environments the users have access to (production vs. staging), the users variable simply becomes a list of usernames, rather than a map.

--- groups.tf
+++ groups.tf
@@ -1,8 +1,6 @@
-# An IAM group for assessment images managers in the Images accounts.
+# An IAM group for assessment images managers in the Images account.
 resource "aws_iam_group" "assessment_images_managers" {
   provider = aws.users
 
-  for_each = local.assessment_images_managers_group_name
-
-  name = each.value
+  name = var.assessment_images_managers_group_name
 }
--- variables.tf
+++ variables.tf
@@ -11,9 +11,9 @@ variable "terraform_state_bucket" {
 }
 
 variable "users" {
-  description = "A map whose keys are the usernames of each user that is allowed to manage assessment images and whose values are lists of the environments each respective user can manage. Example: { \"firstname1.lastname1\" = [\"production\", \"staging\"], \"firstname2.lastname2\" = [\"production\"], \"firstname3.lastname3\" = [\"staging\"] }"
+  description = "A list of usernames that are allowed to manage assessment images. Example: [ \"firstname1.lastname1\", \"firstname2.lastname2\", \"firstname3.lastname3\" ]"
   nullable    = false
-  type        = map(list(string))
+  type        = list(string)
 }
 
 # ------------------------------------------------------------------------------
@@ -24,7 +24,7 @@ variable "users" {
 
 variable "assessment_images_managers_group_name" {
   default     = "assessment_images_managers"
-  description = "The base name of the group to be created for assessment images manager users in each Images account. This value has the environment name appended to it for each environment."
+  description = "The name of the group to be created for assessment images manager users in the Images account."
   nullable    = false
   type        = string
 }
@@ -38,7 +38,7 @@ variable "assume_images_assessmentimagesbucketfullaccess_policy_description" {
 
 variable "assume_images_assessmentimagesbucketfullaccess_policy_name" {
   default     = "Images-AssumeAssessmentImagesBucketFullAccess"
-  description = "The base name to assign the IAM policies that allow assumption of the role that allows full access to the assessment images bucket in an Images account. This value has the environment name appended to it for each environment."
+  description = "The name to assign the IAM policy that allows assumption of the role that allows full access to the assessment images bucket in the Images account."
   nullable    = false
   type        = string
 }
